27172f7b5d86d51b1690ce479444297d35cfba5d
Zuellni <123005779+Zuellni@users.noreply.github.com>
2024-08-17 22:11:32 +0200

    Some more qol

--- crop.py
+++ crop.py
@@ -1,22 +1,22 @@
 import random
+import string
 from argparse import ArgumentParser
 from pathlib import Path
-from string import punctuation
 from uuid import uuid4
 from warnings import simplefilter
 
 simplefilter("ignore")
 
 parser = ArgumentParser()
-parser.add_argument("-i", "--input_dir", type=Path, default="in")
-parser.add_argument("-o", "--output_dir", type=Path, default="out")
+parser.add_argument("-i", "--input", type=Path, default=".")
+parser.add_argument("-o", "--output_dir", type=Path, default=".")
 parser.add_argument("-c", "--crop", type=int, default=64)
-parser.add_argument("-d", "--seed", type=int, default=0)
 parser.add_argument("-a", "--adjust", action="store_true")
 parser.add_argument("-f", "--flip", action="store_true")
 parser.add_argument("-j", "--jpeg", action="store_true")
 parser.add_argument("-s", "--shuffle", action="store_true")
 args = parser.parse_args()
+
 
 from rich.progress import (
     BarColumn,
@@ -26,6 +26,9 @@ from rich.progress import (
     TimeElapsedColumn,
     TimeRemainingColumn,
 )
+from torchvision import io
+from torchvision import transforms as T
+from torchvision.transforms import functional as TF
 
 progress = Progress(
     TextColumn("{task.description}"),
@@ -37,31 +40,19 @@ progress = Progress(
     TimeRemainingColumn(),
 )
 
-random.seed(args.seed)
-input_dir = args.input_dir
 output_dir = args.output_dir
-crop = args.crop
-files = []
-
-for ext in ("avif", "bmp", "jpeg", "jpg", "png", "webp"):
-    files.extend(input_dir.glob(f"*.{ext}"))
-
 output_dir.mkdir(parents=True, exist_ok=True)
-before = len(files)
-digits = len(str(before))
-after = 0
+crop = args.crop
 
-
-from torchvision import io
-from torchvision import transforms as T
-from torchvision.transforms import functional as TF
+if (input := args.input).is_dir():
+    suffixes = (".avif", ".bmp", ".jpeg", ".jpg", ".png", ".webp")
+    files = [f for f in input.glob("*.*") if f.suffix in suffixes]
+else:
+    files = [input]
 
 
 def save_image(image, adjust=False):
-    global after
-
     output = uuid4().hex[:16]
-    after += 1
 
     if adjust:
         brightness = round(random.uniform(0.9, 1.1), 2)
@@ -77,18 +68,20 @@ def save_image(image, adjust=False):
     image = image.cpu()
 
     if args.jpeg:
-        io.write_jpeg(image, str(output_dir / f"{output}.jpg"), quality=100)
+        io.write_jpeg(image, output_dir / f"{output}.jpg", quality=100)
     else:
-        io.write_png(image, str(output_dir / f"{output}.png"), compression_level=0)
+        io.write_png(image, output_dir / f"{output}.png", compression_level=0)
 
     return output
 
 
 def save_text(text, output, shuffle=False):
     output = output_dir / f"{output}.txt"
+    text = "\n".join([t for t in text.splitlines() if t])
+    text = " ".join(text.split())
 
     if shuffle:
-        text = text.strip(punctuation).strip()
+        text = text.strip(string.punctuation + string.whitespace).strip()
         list = [t.strip() for t in text.split(",")]
         random.shuffle(list)
         output.write_text(", ".join(list), encoding="utf-8")
@@ -100,15 +93,15 @@ with progress as p:
     task = p.add_task("Cropping", total=len(files))
 
     for file in files:
-        image = io.read_image(str(file), io.ImageReadMode.RGB).cuda()
+        image = io.read_image(file, io.ImageReadMode.RGB).cuda()
         height, width = image.shape[1:3]
         min_size = min(height, width) // crop * crop
         max_size = max(height, width) // crop * crop
         max_size = max_size + (crop if max_size == min_size else 0)
         text = ""
 
-        if (input := input_dir / f"{file.stem}.txt").is_file():
-            text = input.read_text(encoding="utf-8")
+        if (caption := file.parent / f"{file.stem}.txt").is_file():
+            text = caption.read_text(encoding="utf-8")
 
         base = TF.resize(
             image,
--- florence.py
+++ florence.py
@@ -17,14 +17,14 @@ parser.add_argument("-o", "--output_dir", type=Path, default=".")
 parser.add_argument("-m", "--model", type=str, default="microsoft/Florence-2-large-ft")
 parser.add_argument("-t", "--task", type=str, choices=list(tasks.keys()), default="mdc")
 parser.add_argument("-T", "--tokens", type=int, default=256)
-parser.add_argument("-B", "--beams", type=int, default=3)
+parser.add_argument("-b", "--beams", type=int, default=4)
 parser.add_argument("-d", "--device", type=str, default="cuda")
 parser.add_argument("-D", "--dtype", type=str, default="float16")
 args = parser.parse_args()
 
 import torch
-from PIL import Image
 from rich import print
+from torchvision import io
 from transformers import AutoModelForCausalLM, AutoProcessor
 
 input = args.input
@@ -38,7 +38,7 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map=device,
     torch_dtype=dtype,
     trust_remote_code=True,
-).eval()
+)
 
 processor = AutoProcessor.from_pretrained(
     pretrained_model_name_or_path=args.model,
@@ -48,27 +48,28 @@ processor = AutoProcessor.from_pretrained(
 )
 
 if input.is_dir():
-    suffixes = (".bmp", ".jpeg", ".jpg", ".png", ".webp")
+    suffixes = (".avif", ".bmp", ".jpeg", ".jpg", ".png", ".webp")
     files = [f for f in input.glob("*.*") if f.suffix in suffixes]
 else:
     files = [input]
 
-with torch.inference_mode():
-    for file in files:
-        text = tasks[args.task]
-        images = Image.open(file).convert("RGB")
-        inputs = processor(text=text, images=images)
-        inputs = inputs.to(device=device, dtype=dtype)
+for file in files:
+    task = tasks[args.task]
+    image = io.read_image(file, io.ImageReadMode.RGB)
+    inputs = processor(text=task, images=image)
+    inputs = inputs.to(device=device, dtype=dtype)
 
-        output_ids = model.generate(
-            input_ids=inputs["input_ids"],
-            pixel_values=inputs["pixel_values"],
-            do_sample=False,
-            early_stopping=False,
-            max_new_tokens=args.tokens,
-            num_beams=args.beams,
-        )[0]
+    output = model.generate(
+        input_ids=inputs["input_ids"],
+        pixel_values=inputs["pixel_values"],
+        do_sample=False,
+        early_stopping=False,
+        max_new_tokens=args.tokens,
+        num_beams=args.beams,
+    )[0]
 
-        output = processor.decode(output_ids, skip_special_tokens=True)
-        (output_dir / f"{file.stem}.txt").write_text(output, encoding="utf-8")
-        print(f'File: "{file.name}"\nOutput: "{output}"\n')
+    text = processor.decode(output, skip_special_tokens=True)
+    text = "\n".join([t for t in text.splitlines() if t])
+    text = " ".join(text.split())
+    (output_dir / f"{file.stem}.txt").write_text(text, encoding="utf-8")
+    print(f'File: "{file.name}"\nOutput: "{text}"\n')
